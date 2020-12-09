6d0a919824a69379a72b4e07497b9d4d171ddb6c
spaceC00kie <kirstie317@gmail.com>
2020-12-08 22:03:58 -0500

    arrow function with incrementing

---
+++ practice-05.js
@@ -0,0 +1,6 @@
+//given an array of integers return an array of integers where each element is increased by 2
+//for ex if [1, 2, 3] then output should be [3, 4, 5]
+
+const addTwo = (arr) => arr.map((num) => num + 2)
+
+console.log(addTwo([2, 3, 4]))
